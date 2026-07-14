f430b1468458e3abe921269a61537b2692263a65
Chinmay Mhatre <chinmaymhatre12@gmail.com>
2026-07-14 11:50:45 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const map = new Map()
+        const output = []
+        for (let num of nums) {
+            map.set(num, (map.get(num) || 0) + 1);
+        }
+
+        const buckets = new Map()
+        for (let[key,value] of map.entries()){
+        if (!buckets.has(value)) {
+          buckets.set(value, []);
+         }
+        buckets.get(value).push(key);
+        }
+        let bucketLength = nums.length
+        while (bucketLength > 0 && output.length < k ){
+            let value = buckets.get(bucketLength)
+            if(value){
+               output.push(...value)
+                
+            }
+            
+            bucketLength --
+        }
+        return output.splice(0,k)
+    }
+}
